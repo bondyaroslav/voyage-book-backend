9b7e9876a13144babc11826a3304404456492e4a
bondyaroslav <bondy2526@gmail.com>
2025-09-05 00:12:33 +0300

    Add Docker

--- src/controllers/postsController.ts
+++ src/controllers/postsController.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from "express";
 import {Post} from "../models/Post";
 
-export const getAllPosts = async (res: Response) => {
+export const getAllPosts = async (req: Request, res: Response) => {
     try {
         const posts = await Post.find().sort({createdAt: -1});
         res.status(200).json(posts);
